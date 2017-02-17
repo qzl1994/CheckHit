be1156268b5178bf006d1ffcfa23ba813c3011c0
qzl1994 <305165598@qq.com>
2017-02-17 17:58:54 +0800

    扇形平面与球体的碰撞检测

---
+++ Assets/CheckHit/_Scripts/CheckHit.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckHit : MonoBehaviour
+{
+    public GameObject _sphere;
+    public GameObject _sector;
+    public float _radius;
+    public Button checkButton;
+    public Text checkText;
+
+    void Awake()
+    {
+        _sphere.transform.localScale *= _radius;
+
+        checkButton.onClick.AddListener(Check);
+    }
+
+    public void Check()
+    {
+        bool result = _CheckHit(_sphere, _sector);
+
+        if (result)
+        {
+            checkText.text = "相交";
+        }
+        else
+        {
+            checkText.text = "不相交";
+        }
+    }
+
+    /// <summary>
+    /// 返回V向量在 V1和V2平面上的投影向量
+    /// </summary>
+    /// <param name="v1"></param>
+    /// <param name="v2"></param>
+    /// <param name="v"></param>
+    /// <returns></returns>
+    public Vector3 GetProjection(Vector3 v1,Vector3 v2,Vector3 v)
+    {
+        Vector3 n = Vector3.Cross(v1, v2).normalized;//V1,V2平面的单位法向量
+
+        return v - Vector3.Dot(v, n) * n;
+    }
+
+    public bool _CheckHit(GameObject sphere, GameObject sector)
+    {
+        bool check = false;
+
+        float sphereRadius = _radius / 2;//球半径
+        Vector3 o = sphere.transform.localPosition;//球心
+
+        Sector sectorObj = sector.GetComponent<Sector>();
+        //扇形的3个顶点
+        Vector3 a = sectorObj.GetVertex(0);
+        Vector3 b = sectorObj.GetVertex(1);
+        Vector3 c = sectorObj.GetVertex(2);
+        float angleDegree = sectorObj._angleDegree;//扇形夹角
+        float angle = Mathf.Deg2Rad * angleDegree / 2;//半角弧度
+        float sectorRadius = sectorObj._radius;//扇形长度
+
+        //Debug.Log("A:" + a + ",B:" + b + ",C:" + c);
+
+        //扇形平面
+        Plane sectorPlane = new Plane(a, b, c);
+        float distancePointToPlane = Mathf.Abs(sectorPlane.GetDistanceToPoint(o));
+        Debug.Log("球心到扇形平面距离：" + distancePointToPlane);
+
+        if(distancePointToPlane > sphereRadius)
+        {
+            check = false;
+        }
+        else
+        {
+            Vector3 ab = b - a;
+            Vector3 ac = c - a;
+            Vector3 ao = o - a;
+            Vector3 u = ( ab + ac ).normalized;//扇形方向上的单位向量
+            Vector3 aoo = GetProjection(ab, ac, ao);//扇形原点到球心向量在扇形平面上的投影向量
+            Debug.Log("扇形到球心向量在扇形平面的投影：" + aoo);
+
+            float sqrRadius = Mathf.Sqrt(( sphereRadius * sphereRadius ) - ( ao - aoo ).sqrMagnitude);//扇形平面横切球体出来的圆的半径
+            Debug.Log("横切圆的半径:" + sqrRadius);
+
+            float distancePointToPoint = aoo.sqrMagnitude;//扇形原点到球心投影的距离平方
+            Debug.Log("球心到扇形原点距离：" + Mathf.Sqrt(distancePointToPoint));
+
+            if (distancePointToPoint > ( sectorRadius + sqrRadius ) * ( sectorRadius + sqrRadius ))
+            {
+                check = false;
+            }
+            else
+            {
+                float px = Vector3.Dot(aoo, u);
+
+                if (px > ( aoo.magnitude * Mathf.Cos(angle) ))
+                {
+                    check = true;
+                }
+                else
+                {
+                    Vector3 oo = new Vector3(aoo.x + a.x, aoo.y + a.y, aoo.z + a.z);
+
+                    Debug.Log("横切圆圆心：" + oo);
+
+                    float disAB = SegmentPointSqrDistance(a, b, oo);
+                    float disAC = SegmentPointSqrDistance(a, c, oo);
+                    Debug.Log("横切圆圆心到ab线段距离：" + disAB);
+                    Debug.Log("横切圆圆心到ac线段距离：" + disAC);
+
+                    if (( disAB > ( sqrRadius * sqrRadius ) ) && ( disAC > ( sqrRadius * sqrRadius ) ))
+                    {
+                        check = false;
+                    }
+                    else
+                    {
+                        check = true;
+                    }
+                }
+            }   
+        }
+
+        return check;
+    }
+
+    /// <summary>
+    /// 返回x点到xo,u线段的最短距离
+    /// </summary>
+    /// <param name="x0"></param>
+    /// <param name="u"></param>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public float SegmentPointSqrDistance(Vector3 x0, Vector3 u, Vector3 x)
+    {
+        float t = Vector3.Dot(x - x0, u) / u.sqrMagnitude;
+        return ( x - ( x0 + Mathf.Clamp(t, 0, 1) * u ) ).sqrMagnitude;
+    }
+}
---
+++ Assets/CheckHit/_Scripts/Sector.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+public class Sector : MonoBehaviour
+{
+    public float _radius;
+    public float _angleDegree;
+    public int _segments;
+    private Mesh sectorMesh;
+
+    void Awake()
+    {
+        sectorMesh = CreateMesh(_radius, _angleDegree, _segments);
+
+        GetComponent<MeshFilter>().mesh = sectorMesh;
+    }
+
+    /// <summary>
+    /// 得到扇形坐标，0:圆点，1:左顶点，2:右顶点
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns></returns>
+    public Vector3 GetVertex(int i)
+    {
+        Vector3 v = gameObject.transform.localPosition;
+
+        float currAngle = Mathf.Deg2Rad * _angleDegree / 2;
+
+        switch (i)
+        {
+            case 0:
+                break;
+            case 1:
+                v += new Vector3(Mathf.Cos(currAngle) * _radius, 0, Mathf.Sin(currAngle) * _radius);
+                break;
+            case 2:
+                v += new Vector3(Mathf.Cos(-currAngle) * _radius, 0, Mathf.Sin(-currAngle) * _radius);
+                break;
+        }
+
+        return v;
+    }
+
+
+    /// <summary>
+    /// 创建扇形的Mesh
+    /// </summary>
+    /// <param name="radius"></param>
+    /// <param name="angleDegree"></param>
+    /// <param name="segments"></param>
+    /// <returns></returns>
+    private Mesh CreateMesh(float radius,float angleDegree,int segments)
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "sector";
+
+        Vector3[] vertices = new Vector3[segments + 2];
+        //vertices[0] = sector.transform.localPosition;
+        vertices[0] = new Vector3(0, 0, 0);
+
+        Debug.Log("扇形圆点：" + vertices[0]);
+
+        float angle = Mathf.Deg2Rad * angleDegree;
+        float currAngle = angle / 2;
+        float deltaAngle = angle / segments;
+        for (int i = 1; i < vertices.Length; i++)
+        {
+            vertices[i] = new Vector3(Mathf.Cos(currAngle) * radius, 0, Mathf.Sin(currAngle) * radius) + vertices[0];
+            currAngle -= deltaAngle;
+        }
+
+        Debug.Log("顶点坐标：" + vertices[1] + "," + vertices[segments + 1]);
+
+        int[] triangles = new int[segments * 3];
+        for (int i = 0, vi = 1; i < triangles.Length; i += 3, vi++)
+        {
+            triangles[i] = 0;
+            triangles[i + 1] = vi;
+            triangles[i + 2] = vi + 1;
+        }
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        return mesh;
+    }
+}
